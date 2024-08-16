76a3aa2360a6a53622fbb987899b04e6b8cf5eb3
unknown <mariamgevorgyan9@gmail.com>
2024-08-16 22:02:47 +0200

    base app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
+import { UserInputComponent } from './components/user-input/user-input.component';
+import { InvestmentResultsComponent } from "./components/investment-results/investment-results.component";
+import {type InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  resultsData=signal<{
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number
+  }[]|undefined>(undefined);
+
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    console.table(data);
+    console.log(data);
+    console.log(JSON.stringify(data, null, 2));
+
+    const { expectedReturn, initialInvestment, duration, annualInvestment } =
+      data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    this.resultsData.set(annualData);
+  }
+}
